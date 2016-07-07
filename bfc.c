694690171cf7d944b5c54a96d280f1714cad5945
John Kastner <john.h.kastner@gmail.com>
2016-07-06 23:34:55 -0400

    Using pointer arithmatic instead of array acceses

--- bfc.c
+++ bfc.c
@@ -36,37 +36,38 @@ char call_exit[]  = {0xb8,0x01,0x00,0x00,0x00, /*mov   eax,  1     */
 
 int main(int argc, char **argv){
     char text[MAX_BIN_LEN];
+    char *txt_ptr = text;
+
     Elf32_Off entry;
 
     entry = org + sizeof(Elf32_Ehdr) + 1 * sizeof(Elf32_Phdr);
 
     char input;
-    int text_len = 0;
     while((input = getchar()) != -1){
         switch(input){
             case '>':
-                memcpy(text + text_len,move_right,sizeof(move_right));
-                text_len += sizeof(move_right);
+                memcpy(txt_ptr,move_right,sizeof(move_right));
+                txt_ptr += sizeof(move_right);
                 break;
             case '<':
-                memcpy(text + text_len,move_left,sizeof(move_left));
-                text_len += sizeof(move_left);
+                memcpy(txt_ptr,move_left,sizeof(move_left));
+                txt_ptr += sizeof(move_left);
                 break;
             case '+':
-                memcpy(text + text_len,inc_cell,sizeof(inc_cell));
-                text_len += sizeof(inc_cell);
+                memcpy(txt_ptr,inc_cell,sizeof(inc_cell));
+                txt_ptr += sizeof(inc_cell);
                 break;
             case '-':
-                memcpy(text + text_len,dec_cell,sizeof(dec_cell));
-                text_len += sizeof(dec_cell);
+                memcpy(txt_ptr,dec_cell,sizeof(dec_cell));
+                txt_ptr += sizeof(dec_cell);
                 break;
             case '.':
-                memcpy(text + text_len,print_char,sizeof(print_char));
-                text_len += sizeof(print_char);
+                memcpy(txt_ptr,print_char,sizeof(print_char));
+                txt_ptr += sizeof(print_char);
                 break;
             case ',':
-                memcpy(text + text_len,read_char,sizeof(read_char));
-                text_len += sizeof(read_char);
+                memcpy(txt_ptr,read_char,sizeof(read_char));
+                txt_ptr += sizeof(read_char);
                 break;
             case '[':
             case ']':
@@ -74,8 +75,8 @@ int main(int argc, char **argv){
         }
     }
 
-    memcpy(text + text_len,call_exit,sizeof(call_exit));
-    text_len += sizeof(call_exit);
+    memcpy(txt_ptr,call_exit,sizeof(call_exit));
+    txt_ptr += sizeof(call_exit);
 
     Elf32_Ehdr ehdr = {
         {0x7F,'E','L','F',ELFCLASS32,ELFDATA2LSB,EV_CURRENT,0,0,0,0,0,0,0,0,0},
@@ -99,16 +100,16 @@ int main(int argc, char **argv){
         0,
         org,
         org,
-        sizeof(Elf32_Ehdr) + sizeof(Elf32_Phdr) + text_len,
-        sizeof(Elf32_Ehdr) + sizeof(Elf32_Phdr) + text_len,
+        sizeof(Elf32_Ehdr) + sizeof(Elf32_Phdr) + (txt_ptr - text),
+        sizeof(Elf32_Ehdr) + sizeof(Elf32_Phdr) + (txt_ptr - text),
         PF_R | PF_X,
         0x1000,
     };
 
-    int out = open("a.out",O_CREAT|O_WRONLY,S_IRWXU);
+    int out = open("a.out",O_CREAT|O_TRUNC|O_WRONLY,S_IRWXU);
     write(out,&ehdr,sizeof(Elf32_Ehdr));
     write(out,&phdr,sizeof(Elf32_Phdr));
 
-    write(out,text,text_len);
+    write(out,text,txt_ptr-text);
     close(out);
 }
